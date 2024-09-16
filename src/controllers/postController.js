50401ededf0adc20f34072cb4d7b340325bc0444
Avesh <aveshhasanfatta1155@gmail.com>
2024-09-16 12:39:40 +0530

    feat : added serch filter for the post in API logic

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -4,9 +4,9 @@ const postService = require("../services/post-service");
 
 //New mehod for fatches the posts based on page number
 const getPosts = async (req, res) => {
-  const { page } = req.query;
+  const { page, search, tags } = req.query;
   try {
-    const posts = await postService.getPostsByPage(page);
+    const posts = await postService.getPostsByPage({ page, search, tags });
     return res.status(200).send(posts);
   } catch (error) {
     return res.status(404).send(error.message);
--- src/services/post-service.js
+++ src/services/post-service.js
@@ -3,7 +3,8 @@ const postRepository = require("../repositories/post-repository");
 // ALL the buisness logic will be here
 
 class PostService {
-  async getPostsByPage(page) {
+  async getPostsByPage(queryObj) {
+    const { page, search, tags } = queryObj;
     try {
       //Maximum Numbers of the posts on single page
       const LIMIT = 8;
@@ -13,8 +14,19 @@ class PostService {
 
       const total = await postRepository.getTotalPostsCount();
 
-      // Prepare the query object here (e.g., filtering posts)
-      const query = {}; // Add conditions if needed (e.g., based on user inputs)
+      // TODO: Need to refactor this because when we search by title and tags both together is not working
+
+      let query = {};
+      // Add search conditionally
+      if (search) {
+        const title = new RegExp(search, "i");
+        query.title = title; // Add title to the query if search is present
+      }
+
+      // Add tags conditionally
+      if (tags) {
+        query.tags = { $in: tags.split(",") }; // Add tags to the query if tags are present
+      }
 
       //.limit method fetches the only number of the posts you want in this case is 8
       //.skip method fetches the post of that only specific page number like example if we want to go in 3rd page we don't want to fetch the posts of the first two pages..
